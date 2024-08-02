fb0c3ca7a88dbe8fdcf48594d442511d79118aac
youssef-rafie212 <youssefelbosaty3@gmail.com>
2024-08-02 17:05:37 +0300

    Fav Lists controller

---
+++ GroceryAPI/Controllers/FavouritesListsController.cs
@@ -0,0 +1,45 @@
+﻿using Core.Services_contracts;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GroceryAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class FavouritesListsController : ControllerBase
+    {
+        private readonly IFavouritesListServices _service;
+
+        public FavouritesListsController(IFavouritesListServices service)
+        {
+            _service = service;
+        }
+
+        [HttpGet("user/{id}")]
+        public async Task<IActionResult> GetForUser(Guid id)
+        {
+            try
+            {
+                return Ok(await _service.GetFavouriteListForUser(id));
+            }
+            catch (Exception ex)
+            {
+                return Problem(ex.Message, statusCode: 400);
+            }
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(Guid id)
+        {
+            try
+            {
+                return Ok(await _service.GetFavouritesListById(id));
+            }
+            catch (Exception ex)
+            {
+                return Problem(ex.Message, statusCode: 400);
+            }
+        }
+    }
+}
